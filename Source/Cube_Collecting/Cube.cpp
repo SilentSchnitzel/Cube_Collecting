905863ca4c5b138a8f91b51ebb4dab0549fd38e2
Chervix <122894453+SilentSchnitzel@users.noreply.github.com>
2023-05-07 18:29:58 -0400

    the cube is now animated

--- Source/Cube_Collecting/Cube.cpp
+++ Source/Cube_Collecting/Cube.cpp
@@ -8,7 +8,6 @@ ACube::ACube()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
-
 }
 
 // Called when the game starts or when spawned
@@ -22,6 +21,29 @@ void ACube::BeginPlay()
 void ACube::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
+	ACube::animation();
 
+}
+
+void ACube::animation()
+{
+	static int counter = 0;
+	counter++;
+	// Get the current rotation of the actor
+	//FRotator CurrentRotation = GetActorRotation();
+
+	FRotator NewRotation = FRotator(0.0f, (float)counter, 0.0f);
+	//FRotator NewRotation = FRotator(0.0f, 45.0f, 0.0f); // for example, a 45 degree Yaw rotation
+	if (GEngine)
+	{
+		if (counter == 2)
+		{
+			GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Purple, TEXT("counter equals 2"));
+		}
+		if (counter == 3)
+			GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Purple, TEXT("counter equals 3"));
+	}
+	
+	SetActorRotation(NewRotation);
 }
 
